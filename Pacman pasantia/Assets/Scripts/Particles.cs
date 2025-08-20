41a5c163db4a87fb0e5b0cbaa47e545935829dfa
Persfone <persfone240@gmail.com>
2025-08-20 09:12:13 -0300

    ya andan las particulas, semana 4 terminada

--- Pacman pasantia/Assets/Scripts/Particles.cs
+++ Pacman pasantia/Assets/Scripts/Particles.cs
@@ -6,18 +6,24 @@ public class Particles : MonoBehaviour
     public float cooldown = 0.5f;
     private float lastTime = 0f;
 
+    // Distancia delante del jugador  
+    public float offsetDistance = 1f;
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Wall") && Time.time - lastTime > cooldown)
         {
             lastTime = Time.time;
 
-            GameObject dust = Instantiate(
-                dustEffectPrefab,
-                collision.contacts[0].point,
-                Quaternion.identity
-            );
+            // Posición al frente del jugador
+            Vector3 spawnPos = transform.position + transform.forward * offsetDistance;
+
+            // Rotar el efecto hacia adelante
+            Quaternion spawnRot = Quaternion.LookRotation(transform.forward);
+
+            GameObject dust = Instantiate(dustEffectPrefab, spawnPos, spawnRot);
             Destroy(dust, 2f);
         }
     }
 }
+
